7c23ca03857f8c961119e002a41224fa76c754e7
qtpsudhakar <sudhakar@qtpsudhakar>
2021-04-16 16:58:45 +0530

    changes

--- TSTests/Wd3OhrmLoginXPath.ts
+++ TSTests/Wd3OhrmLoginXPath.ts
@@ -1,10 +1,8 @@
-import {Actions, Builder, By, until, WebDriver, WebElement} from "selenium-webdriver"
+import {Actions, Builder, By, WebDriver} from "selenium-webdriver"
 import "chromedriver"
 import "geckodriver"
 
 (async function () {
-
-    //open application
 
     //open browser
     let driver:WebDriver = await new Builder().forBrowser("chrome").build();
@@ -17,8 +15,6 @@ import "geckodriver"
     let title:string = await driver.getTitle();
     await console.log(title);
 
-    //Login
-
     //enter user name
     await driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("admin");
 
@@ -30,12 +26,8 @@ import "geckodriver"
 
     await driver.sleep(1000);
     
-    //Add Employee
-
     //click on PIM link
     await driver.findElement(By.xpath("//a[normalize-space()='PIM']")).click();
-
-    await driver.sleep(1000);
 
     //click on add employee link
     await driver.findElement(By.xpath("//a[text()='Add Employee']")).click();
@@ -49,39 +41,7 @@ import "geckodriver"
     //click on save
     await driver.findElement(By.xpath("//*[@id='btnSave']")).click();
 
-
-    //Edit Employee
-    await (await driver.findElement(By.xpath("//input[@value='Edit']"))).click();
-
-    await (await driver.findElement(By.id("personal_optGender_1"))).click();
-
-    await (await driver.findElement(By.xpath("//select[@id='personal_cmbNation']/option[text()='Indian']"))).click();
-    
-    //selecting checkbox
-    let chkSmoker:WebElement =await driver.findElement(By.id("personal_chkSmokeFlag"));
-    if (!await chkSmoker.isSelected()) {
-        await chkSmoker.click();
-    }
-
-    //click on save
-    (await driver.findElement(By.id("btnSave"))).click();
-
-    //wait for element to present
-    await driver.wait(until.elementLocated(By.xpath("//input[@id='btnSave' and @value='Edit']")),10000);
-    
-    // await driver.sleep(3000);
-
-    let empId =await (await driver.findElement(By.id("personal_txtEmployeeId"))).getAttribute("value");
-    //click on employee list
-    await (await driver.findElement(By.linkText("Employee List"))).click();
-
-    await driver.sleep(2000);
-
-    await (await driver.findElement(By.xpath("//a[text()='"+empId+"']/../..//input"))).click();
-
-    await (await driver.findElement(By.id("btnDelete"))).click();
-
     //close browser
-    // await driver.quit()
+    await driver.quit()
 
 })();
